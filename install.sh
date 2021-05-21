3c685fe337958bced2ae13e043fdf1c2270fff7b
hatsusato <2765630+hatsusato@users.noreply.github.com>
2021-05-21 09:53:21 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,20 +2,28 @@
 
 set -eu
 
+readonly TOP_DIR=${BASH_SOURCE%/*}
+
+backup() {
+  local patch=$TOP_DIR/${src##*/}.patch
+  local -a diff=(diff -u)
+  diff+=(--label "$src")
+  diff+=(--label "$src")
+  diff+=("$src" "$dst")
+  "${diff[@]}" &>/dev/null && return
+  "${diff[@]}" >"$patch" || :
+}
 copy() {
-  local -i mode=644
-  [[ $src == *.sh ]] && mode=755
-  [[ $src -ot $dst ]] && cp -afTv "$dst" "$src".bak
-  install -C -D -m $mode -v -T "$src" "$dst"
+  local -i mode=444
+  [[ -x $src ]] && mode=555
+  LANG=C install -C -D -m $mode -v -T "$src" "$dst"
 }
 main() {
   local src dst
   for src; do
     [[ -f $src ]] || continue
     dst=$HOME/$src
-    if [[ -f $dst ]]; then
-      diff -q "$src" "$dst" &>/dev/null && continue
-    fi
+    [[ -f $dst ]] && backup
     copy
   done
 }
